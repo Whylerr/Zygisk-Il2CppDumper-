89e97495f839511b0231367d76b54a262183495c
Whylerr <nltungg10@gmail.com>
2026-06-30 11:22:48 -0700

    Add files via upload

---
+++ game.h
@@ -0,0 +1,12 @@
+//
+// Created by Perfare on 2020/7/4.
+//
+
+#ifndef ZYGISK_IL2CPPDUMPER_GAME_H
+#define ZYGISK_IL2CPPDUMPER_GAME_H
+
+#define GamePackageName "com.dts.freefiremax"
+#define DUMP_DELAY 15000
+
+#endif //ZYGISK_IL2CPPDUMPER_GAME_H
+
